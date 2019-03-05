78ed3bd99b8ae2f247dece11c1a5b29d0103d941
gymfox <solod.andrew93@gmail.com>
2019-03-05 12:12:18 +0200

    Fixed conflict

--- src/com/gymfox/entities/Main.java
+++ src/com/gymfox/entities/Main.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static com.gymfox.databases.Entity.setDatabase;
+import static com.gymfox.entities.Entity.setDatabase;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException, Entity.InvalidIdException {
