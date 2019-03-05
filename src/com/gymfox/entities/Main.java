72614175a4be5efc2960a6df7197942b92dd3c84
gymfox <solod.andrew93@gmail.com>
2019-03-05 10:55:15 +0200

    Implement Entities classes in entityTests branch

---
+++ src/com/gymfox/entities/Main.java
@@ -0,0 +1,36 @@
+package com.gymfox.entities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.gymfox.databases.Entity.setDatabase;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, Entity.InvalidIdException {
+        initDatabase();
+        User user = new User(-20);
+        user.setName("test");
+        user.setEmail("test");
+        user.save();
+        printAllUsers();
+    }
+
+    private static void printAllUsers() {
+        for (User user : User.all()) {
+            user.getName();
+            for ( Comment comment : user.getComments() ) {
+                System.out.println(String.format("%s : %s - %s", user.getId(), user.getName(), comment.getText()));
+            }
+        }
+    }
+
+    private static void initDatabase() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/orm", "postgres",
+                    "13121993");
+        setDatabase(connection);
+    }
+}
---
+++ src/com/gymfox/entities/Section.java
@@ -0,0 +1,29 @@
+package com.gymfox.entities;
+
+import java.util.List;
+
+class Section extends Entity {
+    public Section() {
+        super();
+    }
+
+    public Section(Integer id) throws InvalidIdException {
+        super(id);
+    }
+
+    public String getTitle() {
+        return (String) super.getColumn("title");
+    }
+
+    public void setTitle(String value) {
+        super.setColumn("title", value);
+    }
+
+    public List<Category> getCategories() {
+        return super.getChildren(Category.class);
+    }
+
+    public static List<Section> all() {
+        return Entity.all(Section.class);
+    }
+}
